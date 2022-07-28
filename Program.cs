4eb2a910edec3b8e85d4250b76ea70f61a48a8d3
Yolander Mutale <mutale.yola@gmail.com>
2022-07-27 23:43:52 -0400

    Modified

--- Program.cs
+++ Program.cs
@@ -11,26 +11,35 @@ namespace WelcomeMessage
         {
             DispatchServices myObj = new DispatchServices();
             Console.WriteLine(myObj.Message);
+
             {
                 //Just type in the numbers
+                //Need to figure a way to return error if more than 6 numbers are entered
                 Console.WriteLine("What is your MC Number?");
                 int mcNumber = int.Parse(Console.ReadLine());
-
+               
                 {
                     Console.WriteLine("How many years has your authority been active for?");
+
+                    //Authority needs to be over 2 yo if not return error
                     int authorityAge = Convert.ToInt32(Console.ReadLine());
 
                     if (authorityAge < 2)
                     {
                         Console.WriteLine(" Unfortunately your authority is not old enough.");
+
+                        Console.WriteLine("If you feel this was a mistake, please type in below any additional information we can use for your reconsideration.");
+                        string additionalInfo = Console.ReadLine();
+                        int count = additionalInfo.Split(' ').Length;
                     }
                     else if (authorityAge > 2)
                     {
                         Console.WriteLine("Great!");
 
                         {
- //Get contact detalails
+ //Get contact details
                             Console.WriteLine("What is your preferred method of contact? Email or Phone?");
+                            // Type as is : case sensetive
                             string contactMethod = Console.ReadLine();
 
 
