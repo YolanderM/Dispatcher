e72fc921d6540d019684db05d6f7b1c9e604767e
Yolander Mutale <mutale.yola@gmail.com>
2022-07-27 22:11:45 -0400

    New file

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace WelcomeMessage
+{
+    class DispatchServices
+    {
+ //Welcome Message
+        string Message = "We look forward to working with you!";
+
+        private static void Main(string[] args)
+        {
+            DispatchServices myObj = new DispatchServices();
+            Console.WriteLine(myObj.Message);
+            {
+                //Just type in the numbers
+                Console.WriteLine("What is your MC Number?");
+                int mcNumber = int.Parse(Console.ReadLine());
+
+                {
+                    Console.WriteLine("How many years has your authority been active for?");
+                    int authorityAge = Convert.ToInt32(Console.ReadLine());
+
+                    if (authorityAge < 2)
+                    {
+                        Console.WriteLine(" Unfortunately your authority is not old enough.");
+                    }
+                    else if (authorityAge > 2)
+                    {
+                        Console.WriteLine("Great!");
+
+                        {
+ //Get contact detalails
+                            Console.WriteLine("What is your preferred method of contact? Email or Phone?");
+                            string contactMethod = Console.ReadLine();
+
+
+                            if (contactMethod == "Email")
+                            {
+                                Console.WriteLine("Please enter your email address below");
+                                string email = Console.ReadLine();
+                                Console.WriteLine("We will get back to you at " + email);
+                            }
+                           // Area code is needed 
+                            if (contactMethod == "Phone")
+                            {
+                                Console.WriteLine("Please enter your phone  number below");
+                                   long phoneNumber = long.Parse(Console.ReadLine());
+                                Console.WriteLine("We will call you at " + phoneNumber);
+                            }
+
+                        }
+
+                    }
+
+                }
+
+            }
+        }
+    }
+}
